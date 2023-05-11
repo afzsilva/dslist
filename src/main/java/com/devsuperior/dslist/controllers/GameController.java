9250dfb2099e6b7f247942fcdf5ac631615746d7
afzsilva <afzsilva@gmail.com>
2023-05-11 12:19:28 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class GameController {
     @GetMapping()
     public ResponseEntity<List<GameMinDTO>> findAll(){
         return ResponseEntity.ok(gameService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GameDTO> findByID(@PathVariable("id") Long id){
+        return ResponseEntity.ok(gameService.findById(id));
     }
 }
--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -1,9 +1,11 @@
 package com.devsuperior.dslist.service;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +15,13 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         return gameRepository.findAll().stream().map(gameEntity -> new GameMinDTO(gameEntity)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        return gameRepository.findById(id).map(gameEntity-> new GameDTO(gameEntity)).orElseThrow(()->new RuntimeException("Game Não existe na Base"));
     }
 }
